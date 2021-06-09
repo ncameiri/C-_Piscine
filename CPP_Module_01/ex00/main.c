d5f304eb9ed170a4acd01b694e356c5ca149b787
Nuno Cameirinha <ncameiri@c1r9s1.42lisboa.com>
2021-06-09 18:48:20 +0100

    2nd Module

---
+++ CPP_Module_01/ex00/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncameiri <ncameiri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 18:00:33 by ncameiri          #+#    #+#             */
+/*   Updated: 2021/06/09 18:22:12 by ncameiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+/*class test
+{
+    private:
+    std::string ola;
+    public:
+    test();
+    ~test();
+};
+test::test(){
+    return;
+}
+test::~test(){
+    return;
+}*/
+
+int main(){
+    //test* oo = new test;
+    //delete oo;
+    return 1;
+}
