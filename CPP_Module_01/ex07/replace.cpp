8b13509e5856dbe62be4023ce5e3ee2abbacc2e4
ncameiri <nunoocameirinha@gmail.com>
2021-06-21 10:24:31 +0100

    ex7

--- CPP_Module_01/ex07/replace.cpp
+++ CPP_Module_01/ex07/replace.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <sstream>
+//https://www.geeksforgeeks.org/stringstream-c-applications/
+//https://github.com/aorji/42-CPP-pool/blob/master/d01/ex07/replace.cpp
 int main(int argc, char *argv[]){
 
     if(argc == 4)
     {
         std::ifstream ifs(argv[1]);
+        std::string s1 = argv[2];
+        std::string s2 = argv[3];
+        std::string repla = ".replace";
+        std::string _fileout = argv[1] + repla; 
         if(!ifs)
         {
             std::cout<<"invalid file\n";
             return (0);
         }
-        std::string s1 = argv[2];
-        std::string s2 = argv[3];
         if (s1.empty() || s2.empty())
         {
             std::cout<<"you let one or two arguments empty\n";
         }
-        std::string aux = ".replace";
-        std::string _fileout = argv[1] + aux; 
+
         std::string act_word;
         std::ofstream ofs(_fileout);
-        while((ifs>>act_word))
+        //INVENCOES
+        std::stringstream AUX2;
+        AUX2<< ifs.rdbuf();
+        
+        act_word = AUX2.str();
+        std::cout<<act_word[1];
+        ofs<<act_word;
+        /*while((ifs>>act_word))
         {
-            ofs<<s2; 
-
-        }
+            if(act_word == s1)
+                ofs<<s2;
+            else
+                ofs<<act_word;
+        }*/
     }
     else 
         std::cout<<"invalid number of arguments\n";
-return (0);
+    return (0);
 }
