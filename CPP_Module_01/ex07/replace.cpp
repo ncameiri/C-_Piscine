e1d0ccd2d214cd147a1a3a03e1fb1929d4b518f6
Nuno Cameirinha <nunoocameirinha@gmail.com>
2021-06-23 12:54:19 +0100

    ex7 working

--- CPP_Module_01/ex07/replace.cpp
+++ CPP_Module_01/ex07/replace.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+
+//http://ee.usc.edu/~redekopp/cs103/slides/Unit14_Streams.pdf
 //https://www.geeksforgeeks.org/stringstream-c-applications/
 //https://github.com/aorji/42-CPP-pool/blob/master/d01/ex07/replace.cpp
+
 int main(int argc, char *argv[]){
 
     if(argc == 4)
@@ -22,24 +25,61 @@ int main(int argc, char *argv[]){
         {
             std::cout<<"you let one or two arguments empty\n";
         }
-
-        std::string act_word;
+        std::string act_line;
+        std::string act_line_hold;
         std::ofstream ofs(_fileout);
-        //INVENCOES
-        std::stringstream AUX2;
-        AUX2<< ifs.rdbuf();
-        
-        act_word = AUX2.str();
-        std::cout<<act_word[1];
-        ofs<<act_word;
-        /*while((ifs>>act_word))
-        {
-            if(act_word == s1)
-                ofs<<s2;
-            else
-                ofs<<act_word;
-        }*/
-    }
+        getline(ifs, act_line_hold); 
+        while(1)
+       {
+           if(getline(ifs, act_line))
+           {
+                std::stringstream AUX2(act_line_hold);
+                std::string act_word;
+                std::string act_word_hold;
+                AUX2 >> act_word_hold;
+                while(1)
+                {   if(act_word_hold == argv[2])
+                        act_word_hold = argv [3];
+                    if(AUX2>>act_word)
+                    {
+                        ofs<<act_word_hold<<" ";
+                        act_word_hold = act_word;
+                    }
+                    else
+                    {
+                            ofs<<act_word_hold;
+                            break;
+                    }
+                }
+                ofs<<std::endl;
+                act_line_hold = act_line;
+           }
+           else
+           {    
+                std::stringstream AUX2(act_line_hold);
+                std::string act_word;
+                std::string act_word_hold;
+                AUX2 >> act_word_hold;
+                while(1)
+                {   if(act_word_hold == argv[2])
+                        act_word_hold = argv [3];
+                    if(AUX2>>act_word)
+                    {
+                        ofs<<act_word_hold<<" ";
+                        act_word_hold = act_word;
+                    }
+                    else
+                    {
+                            ofs<<act_word_hold;
+                            break;
+                    }
+                }
+               break;
+           }
+           
+       }
+       }
+
     else 
         std::cout<<"invalid number of arguments\n";
     return (0);
