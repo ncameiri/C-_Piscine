e35cdcf3869a22df06ea27420b5347f07ef7b23b
Nuno Cameirinha <nunoocameirinha@gmail.com>
2021-06-29 12:34:51 +0100

    Mod_3ex01

--- CPP_Module_03/ex01/ScavTrap.cpp
+++ CPP_Module_03/ex01/ScavTrap.cpp
@@ -1,11 +1,14 @@
 #include "ScavTrap.hpp"
 ScavTrap::ScavTrap(std::string Name): ClapTrap(Name){
-
+	std::cout << "ScavTrap Constructor called" << std::endl;
 }
 
 ScavTrap::~ScavTrap()  
 {
+	std::cout << "ScavTrap Destructor called" << std::endl;
 }
+
+
 void ScavTrap::guardGate(){
-    
+	std::cout << "ScavTrap in Gate Keeper mode\n";   
 }
--- CPP_Module_03/ex01/main.cpp
+++ CPP_Module_03/ex01/main.cpp
@@ -1,13 +1,15 @@
 #include"ClapTrap.hpp"
+#include"ScavTrap.hpp"
 // https://www.youtube.com/watch?v=oz3f-ms-AJs
 // https://github.com/aorji/42-CPP-pool/blob/master/d03/ex00/FragTrap.cpp
 int main() {
-	ClapTrap hero("Nakayama");
+	/*ClapTrap hero("Nakayama");
     std::cout << "attack:" << std::endl;
 	hero.attack("Chewbacca");
 	hero.takeDamage(5);   
 	hero.takeDamage(5);
     hero.beRepaired(1);
-	hero.beRepaired(9);
+	hero.beRepaired(9);*/
+	ScavTrap Hero("John");
 return 0;
 }
