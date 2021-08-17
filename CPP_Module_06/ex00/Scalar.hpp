689e960e6e37e13fcefa948ea910cad9ea1e69f2
ncameiri <nunoocameirinha@gmail.com>
2021-08-17 10:49:10 +0100

    mod6

---
+++ CPP_Module_06/ex00/Scalar.hpp
@@ -0,0 +1,28 @@
+#ifndef SCALAR_HPP
+# define SCALAR_HPP
+//INT pode ir a char desde 32-126 
+//INT MAX __INT MIN
+//No caso de float ou doble parte inteira para int e se possivel para char
+//Descartar 0's a esquerda
+// inf inff com +-
+//nan e nanf com +-
+//Nao permitir duplos sinais
+class Scalar
+{
+private:
+    /* data */
+public:
+    Scalar();
+    ~Scalar();
+};
+
+Scalar::Scalar()
+{
+}
+
+Scalar::~Scalar()
+{
+}
+
+
+#endif
