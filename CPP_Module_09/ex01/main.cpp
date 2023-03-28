153f5338342296c1241587760daebc1dff09b8ed
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-03-28 19:01:51 +0100

    RPN done

--- CPP_Module_09/ex01/main.cpp
+++ CPP_Module_09/ex01/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     }
 
     std::string numbs_string = argv[1];
-    if (numbs_string.find_first_not_of("0123456789 +-*/") != std::string::npos)
+    if (numbs_string.find_first_not_of(" 0123456789+-*/") != std::string::npos)
     {
         std::cout << "Error" << std::endl;
         return 1;
@@ -25,13 +25,41 @@ int main(int argc, char **argv)
         ss >> temp;
         if (temp.find_first_not_of("0123456789") == std::string::npos)
         {
-            std::cout << "e numer" << std::endl;
+            //std::cout << "e numer" << std::endl;
+            numbs.push(temp.c_str()[0] - 48);
         }
         if (temp.find_first_not_of("+-/*") == std::string::npos)
         {
-            std::cout << "e opera" << std::endl;
+            //std::cout << "e opera" << std::endl;
+            int temp_num2 = numbs.top();
+            numbs.pop();
+            if (numbs.empty())
+            {
+                std::cout << "Error" << std::endl;
+                return 1;
+            }
+            int temp_num1 = numbs.top();
+            numbs.pop();
+            
+            switch(temp.c_str()[0]){
+                case '+':
+                    numbs.push(temp_num1 + temp_num2);
+                    break;
+                case '-':
+                    numbs.push(temp_num1 - temp_num2);
+                    break;
+                 case '/':
+                    numbs.push(temp_num1 / temp_num2);
+                    break;
+                case '*':
+                    numbs.push(temp_num1 * temp_num2);
+                    break;
+                
+            }
         }
+
         std::cout << temp << std::endl;
     }
+    std::cout<< numbs.top();
     return 0;
 }
