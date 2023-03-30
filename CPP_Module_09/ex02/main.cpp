598e4b2caef203cad4d2183f01c484c4ea531cfb
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-03-30 23:15:24 +0100

    pmerge cont..

--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -7,27 +7,41 @@
 #include <cstdlib>
 #define __STDC_LIMIT_MACROS
 #include <stdint.h>
-#include <algorithm> 
+#include <algorithm>
+#include <sys/time.h>
 
-//Usar vector https://cplusplus.com/reference/algorithm/sort/
-//Usar list https://en.cppreference.com/w/cpp/container/list/sort
+// Usar vector https://cplusplus.com/reference/algorithm/sort/
+// Usar list https://en.cppreference.com/w/cpp/container/list/sort
 
-bool check_input(char **argv){
-    int i=1;
-    while(argv[i]){
-        std::string temp= argv[i];
-        if(temp.find_first_not_of("0123456789") != std::string::npos){
+struct TimeStruct{
+     struct timespec beg_cont1;
+     struct timespec beg_cont2;
+     struct timespec end_cont1;
+     struct timespec end_cont2;
+}_times;
+
+bool check_input(char **argv)
+{
+    int i = 1;
+    while (argv[i])
+    {
+        std::string temp = argv[i];
+        if (temp.find_first_not_of("0123456789") != std::string::npos)
+        {
             return true;
         }
-        if(atof(argv[i]) > INT32_MAX || atof(argv[i]) < 0){
+        if (atof(argv[i]) > INT32_MAX || atof(argv[i]) < 0)
+        {
             return true;
         }
-        int z=i;
+        int z = i;
 
         // Check for repeated
-        while(argv[++z]){
-        if(atoi(argv[i]) == atoi(argv[z])){
-            return true;
+        while (argv[++z])
+        {
+            if (atoi(argv[i]) == atoi(argv[z]))
+            {
+                return true;
             }
         }
         i++;
@@ -35,35 +49,63 @@ bool check_input(char **argv){
     return false;
 }
 
-void print_result(void){
-    
+void print_result( void)
+{ 
+   //struct timespec begin = _times.end_cont1;
+// int opt;
+//std::string opt1 = "container 1" time elapsed etc...
+   /*
+    long seconds = end.tv_sec - begin.tv_sec;
+    long nanoseconds = end.tv_nsec - begin.tv_nsec;
+    double elapsed = seconds + nanoseconds*1e-9;*/
 }
 
-void vector_func(int argc, char **argv){
+void vector_func(int argc, char **argv)
+{
     (void)argc;
     (void)argv;
-    
+
     std::vector<int> vect;
-    struct timespec begin, end; 
-    clock_gettime(CLOCK_REALTIME, &begin);
-    for(int i = 1; i < argc ; i++){
+   
+    clock_gettime(CLOCK_REALTIME, &_times.beg_cont1);
+    for (int i = 1; i < argc; i++)
+    {
         vect.push_back(atoi(argv[i]));
     }
-    for (std::vector<int>::iterator it=vect.begin(); it!=vect.end(); ++it)
-    std::cout << ' ' << *it;
-    std::sort (vect.begin(), vect.end());
-    std::cout<<"\nAFTER sort\n";
-       for (std::vector<int>::iterator it=vect.begin(); it!=vect.end(); ++it)
-    std::cout << ' ' << *it;
-   clock_gettime(CLOCK_REALTIME, &end);
+    for (std::vector<int>::iterator it = vect.begin(); it != vect.end(); ++it)
+        std::cout << ' ' << *it;
+    std::sort(vect.begin(), vect.end());
+    std::cout << "\nAFTER sort\n";
+    for (std::vector<int>::iterator it = vect.begin(); it != vect.end(); ++it)
+        std::cout << ' ' << *it;
+    clock_gettime(CLOCK_REALTIME, &_times.end_cont1);
 }
 
-/*void list_func(int argc, char **argv){
+void list_func(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
 
-}*/
+   
+    std::list<int> lis;
+    clock_gettime(CLOCK_REALTIME, &_times.beg_cont2);
+    for (int i = 1; i < argc; i++)
+    {
+        lis.push_back(atoi(argv[i]));
+    }
+    for (std::list<int>::iterator it = lis.begin(); it != lis.end(); ++it)
+        std::cout << ' ' << *it;
+    std::cout << "\nAFTER sort\n";
+    lis.sort();
+    for (std::list<int>::iterator it = lis.begin(); it != lis.end(); ++it)
+        std::cout << ' ' << *it;
+    clock_gettime(CLOCK_REALTIME, &_times.end_cont1);
+
+
+}
 int main(int argc, char **argv)
 {
-     (void)argc;
+    (void)argc;
     (void)argv;
     if (argc < 2 || argc > 3001 || check_input(argv))
     {
@@ -71,14 +113,16 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    vector_func(argc,argv);
-    /*struct timespec begin_cont1, end_cont1; 
+    // vector_func(argc, argv);
+    list_func(argc, argv);
+
+    /*struct timespec begin_cont1, end_cont1;
     clock_gettime(CLOCK_REALTIME, &begin_cont1);
     std::cout<<"";
      usleep(1);
    clock_gettime(CLOCK_REALTIME, &end_cont1);
-   long seconds = end_cont1.tv_sec - begin_cont1.tv_sec;
-    long nanoseconds = end_cont1.tv_nsec - begin_cont1.tv_nsec;
+   long seconds = end.tv_sec - begin.tv_sec;
+    long nanoseconds = end.tv_nsec - begin.tv_nsec;
     double elapsed = seconds + nanoseconds*1e-9;
     //std::setw(10)<<
     std::cout<<std::fixed<<std::setprecision(10)<< elapsed<<"s "<<argv[0];*/
