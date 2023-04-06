982c0e03473af1eb1d1a06d664877f0cea7b8a8f
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-06 16:50:17 +0100

    changes to sorting algo

--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -14,45 +14,104 @@ struct TimeStruct
     struct timespec end_cont2;
 } _times;
 
+std::vector<int> merge_vector(std::vector<int> left, std::vector<int> right)
+{
+    /* if(left.size() > 1 && left[0] > left[1])
+     {
+         int temp = left[0];
+         left[0] = left[1];
+         left[1] = temp;
+     }
+     if(right.size() > 1 && right[0] > right[1])
+     {
+         int temp = right[0];
+         right[0] = right[1];
+         right[1] = temp;
+     }
+     if(*left.begin() > *right.begin()){
+         right.insert(right.end() ,left.begin(), left.end());
+         return right;
+     }
+     else {
+          left.insert(left.end(),right.begin(), right.end());
+         return left;
+     }*/
 
- std::vector<int> merge_vector(std::vector<int> left, std::vector<int> right){
-    if(*left.begin() > *right.begin()){
-        right.insert(right.end(),left.begin(), left.end());
-        return right;
+    std::vector<int> return_vect(left.size() + right.size());
+    int i = -1;
+    unsigned int l = 0, r = 0;
+    while (++i < (int)(left.size() + right.size()))
+    {
+        if (left[l] < right[r])
+        {
+            return_vect[i++] = left[l++];
+        }
+        else
+        {
+            return_vect[i++] = right[r];
+        }
+        if (r == right.size())
+        {
+            while (l != left.size())
+            {
+                return_vect[i++] = left[l++];
+            }
+        }
+        if (l == left.size())
+        {
+            while (r != right.size())
+            {
+                return_vect[i++] = right[r++];
+            }
+        }
     }
-    else {
-         left.insert(left.end(),right.begin(), right.end());
-        return left;
-    }
- }
-
-
-
- std::list<int> merge_list(std::list<int> left, std::list<int> right){
-    if(*left.begin() > *right.begin()){
-        right.merge(left);
-        return right;
-    }
-    else {
-        left.merge(right);
-        return left;
-    }
- }
+    return return_vect;
+}
 
 std::vector<int> merge_sorting_vector(std::vector<int> full_vector)
 {
     std::vector<int>::iterator begin_it = full_vector.begin();
     std::vector<int>::iterator end_it = full_vector.end();
-    std::vector<int>::iterator middle_it = full_vector.begin();
-   
-    int middle_point = full_vector.size() / 2;
-    std::advance(middle_it, middle_point);
-    std::vector<int> left(begin_it, middle_it);
-    std::vector<int> right(middle_it, end_it);
+    if (full_vector.size() < 2)
+        return full_vector;
 
-    if(left.size() < 2 || right.size() < 2)
-        return merge_vector(left,right);
-    return merge_vector(merge_sorting_vector(left),merge_sorting_vector(right));
+    int middle_point = full_vector.size() / 2;
+    /*if(middle_point == 0)
+        return full_vector;*/
+    std::vector<int> left(begin_it, begin_it + middle_point);
+    std::vector<int> right(begin_it + middle_point, end_it);
+
+    /* if(left.size() < 2 || right.size() < 2){
+       if(left.size() > 1 && left[0] > left[1])
+    {
+        int temp = left[0];
+        left[0] = left[1];
+        left[1] = temp;
+        return( merge_vector(merge_sorting_vector(left),right));
+    }
+    if(right.size() > 1 && right[0] > right[1])
+    {
+        int temp = right[0];
+        right[0] = right[1];
+        right[1] = temp;
+        return( merge_vector(merge_sorting_vector(right),left));
+    }
+    }*/
+    return merge_vector(merge_sorting_vector(left), merge_sorting_vector(right));
+}
+
+std::list<int> merge_list(std::list<int> left, std::list<int> right)
+{
+    if (*left.begin() > *right.begin())
+    {
+        right.merge(left);
+        return right;
+    }
+    else
+    {
+        left.merge(right);
+        return left;
+    }
 }
 
 std::list<int> merge_sorting_list(std::list<int> full_list)
@@ -60,17 +119,16 @@ std::list<int> merge_sorting_list(std::list<int> full_list)
     std::list<int>::iterator begin_it = full_list.begin();
     std::list<int>::iterator end_it = full_list.end();
     std::list<int>::iterator middle_it = full_list.begin();
-   
+
     int middle_point = full_list.size() / 2;
     std::advance(middle_it, middle_point);
     std::list<int> left(begin_it, middle_it);
     std::list<int> right(middle_it, end_it);
 
-    if(left.size() < 2 || right.size() < 2)
-        return merge_list(left,right);
-    return merge_list(merge_sorting_list(left),merge_sorting_list(right));
+    if (left.size() < 2 || right.size() < 2)
+        return merge_list(left, right);
+    return merge_list(merge_sorting_list(left), merge_sorting_list(right));
 }
-
 
 bool check_input(char **argv)
 {
@@ -128,11 +186,13 @@ void vector_func(int argc, char **argv)
     {
         vect.push_back(atoi(argv[i]));
     }
-    std::sort(vect.begin(), vect.end());
+    if (vect.size() % 2 != 0)
+        vect.insert(vect.begin(), -1);
+    std::vector<int> res = merge_sorting_vector(vect);
     clock_gettime(CLOCK_REALTIME, &_times.end_cont1);
     // Print after Sort Without spending time
     std::cout << "\nAfter:  ";
-    for (std::vector<int>::iterator it = vect.begin(); it != vect.end(); ++it)
+    for (std::vector<int>::iterator it = res.begin(); it != res.end(); ++it)
         std::cout << ' ' << *it;
     std::cout << std::endl;
 }
@@ -146,7 +206,12 @@ void list_func(int argc, char **argv)
     {
         lis.push_back(atoi(argv[i]));
     }
-    merge_sorting_list(lis);
+    if (lis.size() % 2 != 0)
+        lis.push_front(-1);
+    std::list<int> res = merge_sorting_list(lis);
+    std::cout << "\nAfter list:  ";
+    for (std::list<int>::iterator it = res.begin(); it != res.end(); ++it)
+        std::cout << ' ' << *it;
     clock_gettime(CLOCK_REALTIME, &_times.end_cont2);
 }
 int main(int argc, char **argv)
